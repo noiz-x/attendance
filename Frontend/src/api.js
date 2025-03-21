6947abf903ac65b954f0d5d04043dd5e06b27246
noiz-x <oloyedeelijah23@gmail.com>
2025-03-21 17:14:38 +0100

    Update API base URL for authentication to point to accounts endpoint

--- Frontend/src/api.js
+++ Frontend/src/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE_URL = "http://127.0.0.1:8000/dj-rest-auth/";
+const API_BASE_URL = "http://127.0.0.1:8000/accounts/";
 
 export const loginUser = async (username, password) => {
   try {
